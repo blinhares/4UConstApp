c632178d711ba2dc169b4323eb66c6019dbcdc90
blinhares <b_linhares@outlook.com>
2018-06-08 17:30:05 -0300

    adicionado o envio dos arquivos via email bem como um eviso caso n aja nada que enviar

--- buildozer.spec
+++ buildozer.spec
@@ -38,7 +38,7 @@ version = 0.1
 
 # (list) Application requirements
 # comma seperated e.g. requirements = sqlite3,kivy
-requirements = kivy
+requirements = plyer,kivy
 
 # (str) Custom source folders for requirements
 # Sets custom source for any requirements with recipes
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.core.window import Window
 from kivy.factory import Factory
+from plyer import email
 
 Window.softinput_mode = 'below_target' #PARA QUE O TECLADO N ESCONDA OS INPUT
 Window.clearcolor =[1,1,1,1] #ALTERA A COR DE FUNDO PARA BRANCO
@@ -17,7 +18,25 @@ import diarbordpycode #IMPORTAR CODIGO PARA DIARIO DE BORDO
 
 
 class MenuP(Screen): #cria a classe do Menu Principal
-    pass
+    #Enviar dados por email
+    def enviardados(self):
+        try:
+            with open('dadosdb.csv', 'r') as f:
+                destinatario = "brunobarbosa@quatrou.com.br"
+                assunto = "Diario de Bordo"
+                corpodoemail = ""
+                for linha in f:
+                    print(linha)
+                    corpodoemail = corpodoemail + linha
+                print(corpodoemail)
+                limparquivo = open('dadosdb.csv', 'w')
+                email.send(recipient=destinatario,
+                   subject=assunto,
+                   text=corpodoemail)
+        except IOError:
+            popup = Popup(title='Aviso', content=Label(text='Nao ha dados para enviar'),
+                          auto_dismiss=True, size_hint=(.5, .5))
+            popup.open()
 
 class AvisoDb(Popup):
     pass
