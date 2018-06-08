bc716b32b38dac1d2de46df5cc3a4b10f36144f2
blinhares <b_linhares@outlook.com>
2018-06-08 10:59:00 -0300

    alteração de layout e criacao de novo icone

--- diarbordpycode.py
+++ diarbordpycode.py
@@ -14,8 +14,9 @@ Window.softinput_mode = 'below_target' #PARA QUE O TECLADO N ESCONDA OS INPUT
 Window.clearcolor =[1,1,1,1] #ALTERA A COR DE FUNDO PARA BRANCO
 
 class AvisoDb(Popup):
-    #TODO INSERIR COMANDO PARA QUE O BOTAO SALVAR EXECUTE O COMANDO E MUDE DE TELA
     pass
+    #TODO INSERIR COMANDO PARA QUE O BOTAO SALVAR EXECUTE O COMANDO E MUDE DE TELA
+
 
 class DiarBord(Screen): #cria a classe dodiario de bordo
 
@@ -74,8 +75,8 @@ class DiarBord(Screen): #cria a classe dodiario de bordo
         while i < (len(self.dadospadr)-1): # EQUANTO I É MELHOR QUE O TAMANHP DA LEGENDA FAZ
             if self.dadospadr[i].find(self.dados[i]) == 0:
                 validador = validador + 1
-
             i = i + 1
+
         ########## CASO HAJA ERRO NA COMPARAÇÃO DOS DADOS COM A LEJENDA, INFORMAR AO USUÁRIO ############
         if validador > 1:
             popup = Popup(title='Aviso', content=Label(text='Dados Incompletos.\n Favor verificar'),
